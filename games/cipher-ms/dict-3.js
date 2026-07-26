dded70f2eba2d711ff712641681d5939a54c79ac
MrD009 <fidot09@gmail.com>
2026-07-26 23:15:15 +0800

    Add a Bahasa Melayu Cipher edition, and 3/4/5/6-letter modes for both (#22)
    Teka Kata Neon: a separate game in games/cipher-ms/, same rules and chrome
    as the English one, entire interface in Bahasa Melayu Malaysia, with its
    own storage keys so the two keep independent streaks and stats.
    
    The word data is the real work there. The Malay frequency list behind it
    is built from film subtitles, so it mixes Indonesian spellings in with
    Malay (pikir/fikir, menit/minit, musik/muzik, kabar/khabar) and is thick
    with character names. The answer pools are therefore hand-picked in
    standard Malaysian spelling and checked back against the corpus, while the
    guess dictionaries stay permissive.
    
    Word lengths: 3, 4, 5 and 6 in both editions, each a full game of its own
    with its own daily puzzle, saved board, streak and distribution.
    
    The guess counts are measured rather than assumed, and length + 1 turns
    out to be the wrong rule: short words are harder, not easier, because
    three letters give each guess very little to work with. A solver over the
    full 639-word English three-letter pool failed 38% of the time even on six
    guesses, so that pool is capped tight and the counts land at 7/7/6/7.
    Difficulty became a modifier, so Easy means one extra guess at any size.
    
    Dictionaries now load per length on demand, cutting a default five-letter
    round from 150KB gzipped to 40KB. Existing five-letter saves are carried
    into the new suffixed keys once so streaks survive the upgrade.
    
    Also fixes .row having grid-template-columns hardcoded to 5, which wrapped
    a six-letter row onto two lines; the tests counted cells and could not see
    it, and now assert that a row renders on one line.

---
+++ games/cipher-ms/dict-3.js
@@ -0,0 +1,92 @@
+/*
+ * Teka Kata Neon — kamus tekaan bagi perkataan 3 huruf.
+ *
+ * Dimuatkan bila perlu: hanya panjang yang sedang dimainkan diambil.
+ * Jauh lebih luas daripada kumpulan jawapan, kerana menolak perkataan yang
+ * pemain tahu betul adalah perkara paling menjengkelkan dalam permainan ini.
+ * Daripada hermitdave/FrequencyWords (lesen MIT, korpus OpenSubtitles).
+ */
+(window.CIPHER_MS_DICT = window.CIPHER_MS_DICT || {})[3] = `
+  aaa aah aaj aal aao aap aas aba abc abe abg abi abk abo abs abu abx acc ace aci ack acp
+  act acu ada add ade ado adu aek aey afl afo age agh agi ago aha ahh ahk ahn aib aic aid
+  aig aii aik aim ain air ais aja aje aka ake aki akn ako aks aku ala ale alf all alo alp
+  alu ama amc amp amu and ang anh ani anj ank ant anu any aoa aod aoi aom aon apa apb ape
+  api apo app apt apu aqi arc are ari ark arm arn aro arr art asa ash asi ask aso ass ata
+  atc atf ati atm atp ats att atw auf auh aum aun aur auw ave avl awa awk aww axe aya aye
+  ayo ayu azz bab bac bad bae bag bah bai bak bam ban bao bar bas bat bau bay bbb bbc bbm
+  bbq beb bed beg bei bek ben ber bes bet bew bff bfg bhg bhi bhs bhw bia bic bie big bik
+  bil bin bio bip bir bis bit biu biz bkn bla blg blh blm blt blu bmc bmw bmx bnd bnk bnr
+  boa bob boe bog boi bok bol bom bon boo bop bor bos bot bow box boy bps bra bri bro bru
+  bry bsf bsj bsr btb btn btw bua bub bud bue bug buh bui buk bul bum bun bup bur bus but
+  buu buy bwa bye byk bzz cai caj cak can cao cap car cas cat caw cbi cbp cbs ccs ccu cdc
+  cde cdi cdo ced ceh cek cen ceo cer cfc cfo cha che chi cho chp chu cia cic cid cie cif
+  cik cin cip cis ckp cla clu cnn cnp cob cod com coo cop cor cos cow cox coz cpr cpu cre
+  crt cry csi csu cte cua cum cun cup cut cuz cvr cvs daa dab dad dae dag dah dai dak dal
+  dam dan dao dar das dat dav dax day dcp dcu ddt dea deh dek den deo dep der des det dev
+  dew dex dey dgn dgp dgr dia did die dig dii dik dil dim din dio dip dir dis dit dla dll
+  dlm dls dmn dmv dmz dna dns doa doc dod dog doh doj dok dol dom don doo dor dos dow dpr
+  dps dpt dra drp dry dsm dsp dss dtg dtr dua due dug duh dui duk dum dun duo dux dvd dvr
+  dwi dye dzb eac eah eak eat ecg eco ecr edd edo eds eee eeg eel eep egg ego ehe ehh ehm
+  eid eie eii ein eit eja ekg ekk eko ela elf eli ell elm eme emf emm emo emp ems emt emu
+  end eni ent eod epa epi eps era eri erk erm ern err esa ese esp est esu eta eto ets eum
+  eun evo evp ewk eww exr eye faa fac fai fak far fat fax fbi fbl fda fdr feb fed fei fel
+  fen fet few fez fig fin fir fit fix fli flu fly foi fon foo for fou fox frs fry fsb ftb
+  ftj fub fuh fun fur gag gah gai gak gal gam gan gao gap gar gas gat gbh gbs ged gee gel
+  gem gen geo ger get gga ggu gia gie gig gih gil gim gin gio gir glo glu gmp gmt goa gob
+  god goh gol gom gon goo got gov gpa gpm gps gro grr grs gru gta gti gto gtr gua gul gum
+  gun guo gut guy gwi gye gym gyu haa hab hac had hae hah hai hak hal ham han hao hap har
+  has hat hau haw hay hbo hed hee hef heh hei hel hem hen heo hep her hes het hex hey hie
+  hik him hin hip his hit hiu hiv hku hmm hms hoa hob hoc hoe hog hoh hoi hok hon hoo hop
+  hos hot hou how hoy hrs hrt hss hua hub hud hue hug huh hui huk hum hun huo hup hur hut
+  huu hux huy hve hwa hwi hye hyo iad iam ian iao iap ias ibm ibn ibo ibu ice ics icu ide
+  idi ied iet ifa ifl ifs iga iht iie iii ika ike ila ill ima imf imo inc ind inf ing ini
+  inn ino ins iob ion iot iou iow ipa ipo ips iri irl irs irv isd ish isi isn iso isp isr
+  iss ist isu isz ita iti ito its itt itu iut ive ivo iwo iww iya iye iyo jab jad jae jag
+  jai jam jap jar jas jat jax jay jeb jed jee jek jel jem jep jer jet jew jfk jgn jho jia
+  jib jie jil jim jin jip jiu jka jlb jln job joe jog jom jon joo jou jsa jua jug jum jun
+  jus jut jvc jyn kab kad kae kah kak kal kam kan kao kap kar kas kau kaw keg keh kei kek
+  kel kem ken ker kes kev key kfc kgb kho kia kid kie kin kiu kkk kla klu kms knp kob kod
+  koe koh koi kok kol kon koq kor kos kot kou kpd kpe kri krn kru ksi kta ktk ktm ktp ktx
+  kua kue kui kum kun kuo kut kya kyu laa lab lad lae lag lah lai lak lal lam lan lao lap
+  lar las lat lau lav law lax lay lbh lbu lcd ldd led leg leh lei lem len leo ler les let
+  lev lew lex ley lho lie lif lik lim lip lis lit liu lll lmf lni lnn lns lob lod log loh
+  lok lol lon loo los lot low lox lpp lra lsd lsi lsn lts ltu luc lug lui luk lun luo lut
+  lux lve lya lye maa mac mad mah mai mak mal mam man mao map mar mas mat mau mav maz mba
+  mbi mcc mcm mcu mea med mee meh mem men mer mes met mez mgm mhh mhm mic mie mio mit mix
+  mjd mkv mla mlm mma mmf mmg mmh mmk mmm mnt moa mob moc mod moe moh moi mok mol mom mon
+  moo mop mor mou mph mpr mps mri mrk mrs msc msg msj msw mta mtj mtr mtv mua mub mud mug
+  muh mui muj muk mul mum mun muu muy mvp nab nag nah nai nak nam nas naw naz nba nbc nbe
+  nbs ncc nda ned nee neg nek neo net neu new nfb nfl nge ngo nha nhe nhu nia nie nih nii
+  nik nil nim nin niu nix noc nod noe noh noi nok nol non noo nor nos not now nox npr nri
+  nsa nsg nuh nui nun nuo nur nut nya nye nym nyu nyx nzt oak oan oat obi ock ocp oda odd
+  oeh off ofl ohh ohm oho oia oid oii oil oit ojo oka oke okk oku old ole oli omg omi omo
+  one ong oni onn ons oof ooh ooi oom ooo oop oow opa ops orb orc ore org ori orn osa osi
+  oss ost oui our out owe owh owi owl own oww oxy oya oye ozu pac pad pah pai pak pal pam
+  pan pao pap par pas pau pav paw pax pay paz pbb pbc pbs pcp pda pea pee pei pek pel per
+  pes pew pfc pft pha phd phi phk phu pic pid pie pig pil pim pin pip pir pis pit piu pjj
+  pla plc plg pls pms pnb pnh pod poe poh pok pol pom pon poo pop por pos pot pov pow ppd
+  pra prg pro prq psi psk psr pss pst pta ptg pua pub pun pup pus put pwd pym pyo qia qin
+  qiu qrf qrh qrs que qui quo qur rab rad rah rai raj rak ram ran rao rap ras rat raw rdx
+  reb rec red ref reg rel rem ren reo rep res rev rex ria ric rig rik rim rin rio rip rit
+  rmh rna rnb rob roc rod roe rog roh rok rol rom ron roo ror rov rox roy rpg rpm rrf rsa
+  rsj rue ruf rum run rus rut ruu rye ryo ryu saa sab sac sad sae sah sai san sao sap sar
+  sas sat sau saw say sbg scc scr sdf sdg sdh sdr sdu sea seb sec see seg sei sek sel sem
+  sen seo sep ser set sex sgt sha she shh shi shj sho shu shy sia sic sid sie sig sih sik
+  sil sim sin sio sip sir sis sit siu six sja ski sku sky slg slo sls sma smp sms smu sng
+  sni sns sob sof soi sok sol som son soo sos sou sox soy spa spt spy sri srt ssh ssp sst
+  sta std sto stt stu sua sub sud suh sui suk sul sum sun sup sur sut suv sya syd sye syl
+  sys syy tab tac tad tae tag tah tai taj tak tal tam tan tao tap tar tas tat tau taw tay
+  tbd tcp tdd tdk tea tee teg teh tei tej tek tel ten tep ter tes tet tex tey tgh tgk tha
+  the thi thn tho thu thy tia tic tie tig tik til tim tin tio tip tir tko tkp tle tlg tlh
+  tnt toa toc tod toe toh toi tok tol tom ton too top tor tos tot tov tow tox toy tpa tpb
+  tri try tsa tsb tse tsk tsu ttg tua tub tue tuh tuk tum tuo tur tut tuu tux two txl tze
+  tzu uap uar uav ubi ubn uco udo ufc ufo ugd ugh uhf uhh uhl uhm uji uki uks uli ulu umm
+  unc und unh uni uno unr upl ups uri urk url urn usa usb usd use usr uss uth utk uuh uuu
+  uzi vad vai var vas vcr vcs vee vel ver ves vfx vhf vhs via vic vie vip vir vis viu vob
+  von vsi waa wad wah wai wak wan wap war was wat wau waw wax way wba wbc wdc web wee weh
+  wei wen wes wet wey wgt wha whe who why wig wih wii win wiz wmd woa woh woi wok wol won
+  woo wot wow woz wrb wtc wuv wyk xie xin xiu xue xun xxx yaa yag yah yai yak yal yam yan
+  yao yap yau yay yea yee yeh yek yen yeo yep yer yes yet yey yik yim yin yip yis yiu yng
+  yom yoo you yov yow yud yue yuh yui yuk yul yum yun yup yut zag zai zaj zao zap zat zec
+  zed zee zen zeo zep zhi zhu zin zip zod zog zon zoo zqn zum zzz
+`.trim().split(/\s+/);
